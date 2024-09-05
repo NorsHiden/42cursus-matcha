586f15930a4630efe239cedcb617c8fd6f4a4816
Nors <nasroallah.elidrissi@gmail.com>
2024-09-05 22:25:35 +0100

    init

---
+++ api/src/main.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+
+function bootstrap() {
+  const app = express();
+
+  const PORT = process.env.PORT || 3000;
+
+  app.use(express.json());
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Api is Ready!");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+bootstrap();
